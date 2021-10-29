b32edcb80ed72b82cd357ae8a9775516eb9a2d30
AustinxEwell <austin.ewell86@gmail.com>
2021-10-29 09:03:25 -0600

    started created the api/user routes. started the find all user api return

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,9 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/', homeRoutes);
+// router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
+// router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
